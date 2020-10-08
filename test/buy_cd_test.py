827083c472e82322d8693b7b953cca7bfa4d6a33
Tom <thomas.spencer@codurance.com>
2020-10-08 07:47:51 +0100

    Initial commit

---
+++ test/buy_cd_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class CompactDisc(object):
+    def __init__(self, initial_stock):
+        pass
+
+    def get_stock_count(self):
+        pass
+
+    def buy(self, quantity):
+        pass
+
+
+class BuyCdTest(unittest.TestCase):
+    def test_buy_cd_in_stock(self):
+        cd = CompactDisc(10)
+        cd.buy(5)
+        self.assertEqual(5, cd.get_stock_count())
+
+
+if __name__ == '__main__':
+    unittest.main()
