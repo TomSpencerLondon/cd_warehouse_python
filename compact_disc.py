8ef4e7b75ea91d6f13c44c1c2b4838d9129dcc69
Tom <thomas.spencer@codurance.com>
2020-10-08 10:45:55 +0100

    Add get_stock_count and buy in separate CompactDisc class

---
+++ compact_disc.py
@@ -0,0 +1,10 @@
+
+class CompactDisc(object):
+    def __init__(self, initial_stock):
+        self._stock_count = initial_stock
+
+    def get_stock_count(self):
+        return self._stock_count
+
+    def buy(self, quantity):
+        self._stock_count -= quantity
--- test/buy_cd_test.py
+++ test/buy_cd_test.py
@@ -1,15 +1,6 @@
 import unittest
 
 
-class CompactDisc(object):
-    def __init__(self, initial_stock):
-        self._stock_count = initial_stock
-
-    def get_stock_count(self):
-        return self._stock_count
-
-    def buy(self, quantity):
-        self._stock_count -= quantity
 
 
 class BuyCdTest(unittest.TestCase):
